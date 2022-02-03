db0da30497c657ebc4a53ce3dc0ce43c49654b86
Haoyu Yang <haoyu04@gmail.com>
2022-02-03 13:32:47 -0500

    q

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from helpers import connect
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-
+app.secret_key = "dev"
 
 # no cache
 @app.after_request
@@ -21,7 +21,7 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
-app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_PERMANENT"] = True
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
