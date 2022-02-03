2970b79d0cda838ee31b9fcf502297710a7610e3
Haoyu Yang <haoyu04@gmail.com>
2022-02-03 09:26:00 -0500

    flask_session

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from helpers import connect
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
+app.secret_key = "dev"
 
 # no cache
 @app.after_request
